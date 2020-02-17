ce8b14b18461c80ca3d190009f45d1cbf7f50315
Juan Rambal <juanp231100@gmail.com>
2020-02-16 22:53:30 -0500

    middleware

--- app.js
+++ app.js
@@ -1,41 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+const app = require('./middleware/routes');
+const express = require('express');
+const router = express.Router();
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+router.get('/',function(req, res, next) {
+  res.send('respond with a resource');
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+app.listen(3000); //Escuchar en el puerto 3000
 module.exports = app;
---
+++ middleware/routes.js
@@ -0,0 +1,14 @@
+const app = require('./express');
+const routesfolder = './routes/';
+const fs = require('fs');
+
+//Cargar todas las rutas que estén en /routes
+fs.readdirSync(routesfolder).forEach(file => {
+  let filedir = file.toString();
+  filedir=filedir.substring(0,filedir.length-3);
+  let path = `../routes/${filedir}`;
+  let mainrouter = require(path);
+  app.use(`/${filedir}`,mainrouter);
+});
+
+module.exports = app;
