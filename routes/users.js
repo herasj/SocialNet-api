45e1e580a6a9c4c9820baf4607375dec62694885
herasj <herasj@uninorte.edu.co>
2020-02-22 19:05:56 -0500

    Revert "revision"
    This reverts commit 51444343daf3e164f64b89bb756d30875f494822.
    
    Fatal error

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,6 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const controller = require('../controllers/user.controller');
 const authjwt = require('../middleware/jwt');
-const errhelp = require ('../helpers/error-handler');
 /* GET users listing. */
 router.get(
 	'/',
@@ -18,16 +17,8 @@ router.get(
 	}
 );
 router.post('/token', (req, res) => {
-	const rToken = req.body.token;
-	if (rToken == null) return res.sendStatus(401); //Verify refresh token
-	controller
-		.token({ email: req.body.email, token: rToken })
-		.then(() => {
-			authjwt.verifytoken(rToken, res)
-		})
-		.catch((err) => {
-			errhelp.render(err,res);
-		});
+  const rToken = req.body.token;
+  if(rToken==null) return res.sendStatus(401); //Verify refresh token
 });
 router.post('/auth', function(req, res) {
 	const user = { email: req.body.email, pass: req.body.pass }; //Get info from body
@@ -36,17 +27,26 @@ router.post('/auth', function(req, res) {
 	controller
 		.auth(user)
 		.then(() => {
-			res.json({ access_token: access_token, refresh_token: refresh_token }) //After the user is verified the token is sent
+			res.json({ access_token: access_token, refresh_token: refresh_token }); //After the user is verified the token is sent
 		})
 		.catch((err) => {
-			console.error(err)
-			res.send(err)
+			console.error(err); //If there's not user then
+			res.sendStatus(404);
 		});
 });
 
 router.post('/register', function(req, res, next) {
-	const access_token = authjwt.accesstokenexp(req.body.email); //Generate a token with exp
-	const refresh_token = authjwt.refreshtoken(req.body.email); //Generate a refresh token
+	const user = {
+		name: req.body.name,
+		last: req.body.last,
+		phone: req.body.phone,
+		birth: req.body.birth,
+		email: req.body.email,
+		pass: req.body.pass
+	};
+
+	const access_token = authjwt.accesstokenexp(user); //Generate a token with exp
+	const refresh_token = authjwt.refreshtoken(user); //Generate a refresh token
 
 	const veruser = {
 		name: req.body.name,
@@ -63,7 +63,8 @@ router.post('/register', function(req, res, next) {
 			res.json({ access_token: access_token, refresh_token: refresh_token }); //After the user is verified the token is sent
 		})
 		.catch((err) => {
-			res.json(err);
+			console.error(err); //If there's not user then
+			res.sendStatus(err);
 		});
 
 	console.table(req.body);
